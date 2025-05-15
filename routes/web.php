3ebef0cb289664534369de93df379af5d6c82c14
BrayamFernandez <brayam.3333@gmail.com>
2025-05-14 20:02:16 -0700

    Diseño de vista de información detallada de alumno

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,11 @@ Route::get('/calificaciones', function () {
 Route::get('/alumno/{alumnoId}', InfoAlumno::class)
     ->name('alumno.info');
 
+// Nueva ruta para la vista estática de detalle de alumno, apuntando al archivo en livewire
+Route::get('/info-alumno/{id?}', function ($id = null) {
+    return view('livewire.info-alumno');
+})->middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->name('detalle.alumno');
+
 Route::get('/inscribir', function () {
     return view('inscribir');
 })->middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->name('inscribir');
@@ -38,9 +43,9 @@ Route::get('/inscritos', Inscritos::class)
 
 
 Route::middleware([
-    'auth:sanctum',//token autentificacion
-    config('jetstream.auth_session'),//autentificacion
-    'verified',//verificacion de correo electronico
+    'auth:sanctum', //token autentificacion
+    config('jetstream.auth_session'), //autentificacion
+    'verified', //verificacion de correo electronico
 ])->group(function () {
 
 
@@ -48,18 +53,18 @@ Route::middleware([
 
 
     Route::prefix('pase_de_lista')->controller(PaseDeListaController::class)
-    ->group(function () {
-        Route::get('','mostrar');
-        Route::get('{grupo_id}','selectGrupo');
-        Route::get('inasistencia/vino/{alumno_id}','vino');
-        Route::get('inasistencia/falto/{alumno_id}','falto');
-        Route::get('listar/{numero_de_lista}','listar');
-        Route::get('listar/{numero_de_lista}/vino','listarVino');
-        Route::get('listar/{numero_de_lista}/falto','listarFalto');
-    });
-    Route::get('pase_lista',PaseDeLista::class)->middleware(LogPeticion::class);
-    Route::get('catalogo/maestros',CatalogoMaestros::class);
-    Route::get('lista/{grupo_id}',[PdfController::class, 'lista']);
+        ->group(function () {
+            Route::get('', 'mostrar');
+            Route::get('{grupo_id}', 'selectGrupo');
+            Route::get('inasistencia/vino/{alumno_id}', 'vino');
+            Route::get('inasistencia/falto/{alumno_id}', 'falto');
+            Route::get('listar/{numero_de_lista}', 'listar');
+            Route::get('listar/{numero_de_lista}/vino', 'listarVino');
+            Route::get('listar/{numero_de_lista}/falto', 'listarFalto');
+        });
+    Route::get('pase_lista', PaseDeLista::class)->middleware(LogPeticion::class);
+    Route::get('catalogo/maestros', CatalogoMaestros::class);
+    Route::get('lista/{grupo_id}', [PdfController::class, 'lista']);
 });
 
 Route::get('/menu', function () {
@@ -67,10 +72,10 @@ Route::get('/menu', function () {
 })->name('menu');
 
 
-Route::get("w3css",function(){
+Route::get("w3css", function () {
     return view("w3css");
 });
 
-Route::get("bootstrap",function(){
+Route::get("bootstrap", function () {
     return view("bootstrap");
 });
