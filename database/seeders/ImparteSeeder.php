ae8df5a77ecde2403217c97cb56f4addf490f1dc
MRIkerDev <iker.meza.devDev@gmail.com>
2025-05-14 21:10:10 -0700

    Modificacion de seeder para que las materias esten asignada a su grado correspondiente

--- database/seeders/ImparteSeeder.php
+++ database/seeders/ImparteSeeder.php
@@ -4,27 +4,28 @@ namespace Database\Seeders;
 
 use App\Models\Grupo;
 use App\Models\Imparte;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Maestro;
 use App\Models\Materia;
 
 class ImparteSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $maestros = Maestro::all();
         $grupos = Grupo::all();
-        $materias = [];
+
         foreach ($grupos as $grupo) {
-            $materias = Materia::porGrado($grupo->grado);
+            // se asignara solo la materia del mismo grado que el grupo
+            $materias = Materia::where('grado', $grupo->grado)->get();
+
             foreach ($materias as $materia) {
-                Imparte::create(['materia_id'=>$materia->id,
-                                 'grupo_id'=>$grupo->id,
-                                 'maestro_id'=> $maestros->random()->id]);
+                Imparte::create([
+                    'materia_id' => $materia->id,
+                    'grupo_id' => $grupo->id,
+                    //es para que ponga maestros que no tengan materias
+                    'maestro_id' => rand(0, 1) ? $maestros->random()->id : null
+                ]);
             }
         }
     }
