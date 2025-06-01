b2a68dbc0eab5cfe16a3756e1099d56e4b9ace34
Escuela <Escuela@MSI>
2025-06-01 14:28:14 -0700

    Cambios

---
+++ database/seeders/ImparteSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Grupo;
+use App\Models\Imparte;
+use Illuminate\Database\Seeder;
+use App\Models\Maestro;
+use App\Models\Materia;
+
+class ImparteSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $maestros = Maestro::all();
+        $grupos = Grupo::all();
+        
+        foreach ($grupos as $grupo) {
+            // se asignara solo la materia del mismo grado que el grupo
+            $materias = Materia::where('grado', $grupo->grado)->get();
+
+            foreach ($materias as $materia) {
+                Imparte::create([
+                    'materia_id' => $materia->id,
+                    'grupo_id' => $grupo->id,
+                    //es para que ponga maestros que no tengan materias(se cambio la migracion para que sea nulo)
+                    'maestro_id' => rand(0, 1) ? $maestros->random()->id : null
+                ]);
+            }
+        }
+    }
+}
